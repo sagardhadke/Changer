212df4699410084b590b17e48417fdd208a778b2
Sagar Dhadke <peaceunique2003@gmail.com>
2024-04-25 17:02:34 +0530

    1.0 Release

--- app/src/main/java/net/uniquecomputer/mycurrency/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/mycurrency/MainActivity.kt
@@ -1,15 +1,23 @@
 package net.uniquecomputer.mycurrency
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import net.uniquecomputer.mycurrency.databinding.ActivityMainBinding
+import kotlin.time.times
 
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var isCardOneVisible = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +31,65 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        binding.submitBtn.setOnClickListener {
-
+        binding.indiaCard.setOnClickListener {
+            var input = binding.inputEt.text.toString()
+            if (input.equals("") || input.toString().length <= 0) {
+                Toast.makeText(this, "$input is Not Valid", Toast.LENGTH_SHORT).show()
+            } else {
+                convertInr()
+            }
         }
 
+        binding.usaCard.setOnClickListener {
+            var input = binding.inputEt.text.toString()
+            if (input.equals("") || input.toString().length <= 0) {
+                Toast.makeText(this, "$input is Not Valid", Toast.LENGTH_SHORT).show()
+            } else {
+                convertUSd()
+            }
+        }
     }
+
+    private fun convertInr() {
+
+        var input = binding.inputEt.text.toString().toDouble()
+        var getText = findViewById<TextView>(R.id.amount)
+        var getCurrentValue = 83.32
+        var finalValue = input*getCurrentValue
+        binding.amount.visibility = View.VISIBLE
+        animateTextView(getText,finalValue)
+        getText.text = finalValue.toString()
+    }
+
+
+    private fun convertUSd(){
+        var input = binding.inputEt.text.toString().toDouble()
+        var getText = findViewById<TextView>(R.id.amount)
+        var currentUSd = 0.012
+        var finalUsd = currentUSd*input
+        binding.amount.visibility = View.VISIBLE
+        animateTextView(getText,finalUsd)
+        getText.text = finalUsd.toString()
+    }
+
+    private fun animateTextView(textView: TextView, targetValue: Double) {
+        val initialValue = 0.0
+
+        // Create ValueAnimator
+        val valueAnimator = ValueAnimator.ofFloat(initialValue.toFloat(), targetValue.toFloat())
+
+        // Set duration
+        valueAnimator.duration = 500 // Adjust duration as needed
+
+        // Update TextView with animated values
+        valueAnimator.addUpdateListener { animator ->
+            val animatedValue = animator.animatedValue as Float
+            textView.text = animatedValue.toString()
+        }
+
+        // Start animation
+        valueAnimator.start()
+    }
+
 }
+
